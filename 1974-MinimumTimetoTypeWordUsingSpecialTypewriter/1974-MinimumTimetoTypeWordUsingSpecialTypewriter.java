dd992aff1ee9ea20e15ae74a7ba4c64f4e543217
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-03-08 14:02:38 +0530

    File updated at 08/03/2025, 14:02:38

--- 1974-MinimumTimetoTypeWordUsingSpecialTypewriter/1974-MinimumTimetoTypeWordUsingSpecialTypewriter.java
+++ 1974-MinimumTimetoTypeWordUsingSpecialTypewriter/1974-MinimumTimetoTypeWordUsingSpecialTypewriter.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minTimeToType(String word) {
-
-        int seconds = word.length();
-
-        char pointerPosition = 'a';
-
-         for(char targetChar : word.toCharArray()){
-            seconds += Math.min(Math.abs((int)(targetChar - pointerPosition )) , 26 - Math.abs((int)(targetChar - pointerPosition ))) ;
-            pointerPosition = targetChar;
+        int time = 0;
+        char curr = 'a';
+        for(int i=0;i<word.length();i++){
+            char goal = word.charAt(i);
+            int dist = Math.abs(goal - curr);
+            int circ = 26 - dist;
+            time = time + 1 +Math.min(circ,dist);
+            curr = goal;
         }
-       return seconds;
-}
+        return time;
+    }
 }
