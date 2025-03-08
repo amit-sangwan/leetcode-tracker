a35975a51155ff3fa4e0957d688c49ee733508ff
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-03-08 15:50:58 +0530

    File updated at 08/03/2025, 15:50:57

--- 2027-MinimumMovestoConvertString/2027-MinimumMovestoConvertString.java
+++ 2027-MinimumMovestoConvertString/2027-MinimumMovestoConvertString.java
@@ -16,15 +16,9 @@ class Solution {
     public int replace(String str){
 
         if( str.indexOf("X")+2 < str.length() && str.indexOf("X") != -1){
-            System.out.println("if");
-            res += 1;
-            System.out.println(str);
-            System.out.println(str.substring(str.indexOf("X")+3 , str.length()));
-            replace(str.substring(str.indexOf("X")+3 , str.length()));
+             res += 1;
+             replace(str.substring(str.indexOf("X")+3 , str.length()));
         }else if(str.contains("X")){
-            System.out.println("else");
-
- 
             res+=1;
         }
         return res;
