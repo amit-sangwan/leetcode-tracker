e62143e7293b2966ba24037852480d136221fe5d
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-18 01:56:52 +0530

    File updated at 18/08/2025, 01:56:51

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,10 +1,11 @@
-// Last updated: 18/08/2025, 01:54:07
+// Last updated: 18/08/2025, 01:56:51
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
+            int n2 = target - nums[i] ;
             for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
+                if (nums[j] == n2) {
                     return new int[] { i, j };
                 }
             }
