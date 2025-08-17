4c5d9b8a240cc8d7d1a937d178af353f3c562dfd
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-18 01:51:03 +0530

    File updated at 18/08/2025, 01:51:02

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,27 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
-
+// Last updated: 18/08/2025, 01:51:02
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a hash map to store the difference and its index
-        Map<Integer, Integer> numMap = new HashMap<>();
-        
-        // Iterate through the array
-        for (int i = 0; i < nums.length; i++) {
-            // Calculate the difference between the target and the current number
-            int difference = target - nums[i];
-            
-            // Check if the difference exists in the hash map
-            if (numMap.containsKey(difference)) {
-                // If it exists, return the indices
-                return new int[] { numMap.get(difference), i };
+
+        int[] arr = new int[2];
+        int l = nums.length ;
+
+        for (int i = 0; i < l; i++) {
+            int num1 = nums[i];
+
+            for (int j = i + 1; j < l; j++) {
+                int num2 = nums[j];
+
+                if (num1 + num2 == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
             }
-            
-            // Otherwise, add the current number and its index to the hash map
-            numMap.put(nums[i], i);
         }
-        
-        // If no solution is found (though the problem guarantees one), return an empty array
-        return new int[] {};
+            return null ;
     }
 }
