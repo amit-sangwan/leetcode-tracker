76d9e30523502013d1b17ea5e63ee0aaccb8db35
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-18 23:39:11 +0530

    File updated at 18/08/2025, 23:39:10

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,4 +1,4 @@
-// Last updated: 18/08/2025, 23:25:38
+// Last updated: 18/08/2025, 23:39:10
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,27 +11,25 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-              // Dummy node to simplify merging
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+    ListNode dummy = new ListNode();
+    ListNode current = dummy ;
 
-        // Traverse both lists
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                current.next = list1;
-                list1 = list1.next;
-            } else {
-                current.next = list2;
-                list2 = list2.next;
-            }
-            current = current.next;
+    while(list1 != null && list2 != null){
+
+        if(list1.val < list2.val){
+            current.next = list1 ; 
+            list1 = list1.next ;
+        }else{
+            current.next = list2 ;
+            list2 = list2.next ;
         }
-
-        // Append the remaining nodes
-        if (list1 != null) current.next = list1;
-        if (list2 != null) current.next = list2;
-
-        // Return head of merged list
-        return dummy.next;
+        current = current.next; // move current forward
     }
+
+    if(list1 != null){current.next = list1;}
+    if(list2 != null){current.next = list2;}    
+    
+    return dummy.next ;
+    }
+        
 }
