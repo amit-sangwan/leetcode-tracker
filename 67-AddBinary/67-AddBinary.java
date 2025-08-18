77198f589d8ddba74b134ae249b4981d9d6342eb
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-18 12:30:59 +0530

    File updated at 18/08/2025, 12:30:59

--- 67-AddBinary/67-AddBinary.java
+++ 67-AddBinary/67-AddBinary.java
@@ -1,4 +1,4 @@
-// Last updated: 18/08/2025, 12:30:22
+// Last updated: 18/08/2025, 12:30:59
 class Solution {
     public String addBinary(String a, String b) {
 
@@ -10,25 +10,25 @@ class Solution {
         while (carry > 0 || lenA >= 0 || lenB >= 0) {
             int sum = 0;
             sum += carry;
-            System.out.println("index  " + lenA + " : " + lenB);
+         //   System.out.println("index  " + lenA + " : " + lenB);
 
             if (lenA >= 0) {
                 sum += a.charAt(lenA) - '0';
-                System.out.println("a  " + a.charAt(lenA));
+       //         System.out.println("a  " + a.charAt(lenA));
 
                 lenA--;
             }
             if (lenB >= 0) {
                 sum += b.charAt(lenB) - '0';
-                System.out.println("b  " + b.charAt(lenB));
+       //         System.out.println("b  " + b.charAt(lenB));
 
                 lenB--;
             }
-            System.out.println("sum  " + sum);
+       //     System.out.println("sum  " + sum);
 
             sb.append(sum % 2);
             carry = sum / 2;
-            System.out.println("append " + sum % 2 + "carry: " + sum / 2);
+       //     System.out.println("append " + sum % 2 + "carry: " + sum / 2);
         }
         return sb.reverse().toString();
 
