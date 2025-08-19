a8ab002ab98635b8dcb4a80e035a6530a4e360b2
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-19 13:15:36 +0530

    File updated at 19/08/2025, 13:15:35

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,41 +1,50 @@
-// Last updated: 18/08/2025, 23:01:11
+// Last updated: 19/08/2025, 13:15:35
 class Solution {
     public boolean isValid(String s) {
-
         int len = s.length();
-        if (len < 2)
-            return false;
-
         Stack<Character> stack = new Stack();
-
         for (int i = 0; i < len; i++) {
-            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+            System.out.println("*************");
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                System.out.println("Item pushed: " + s.charAt(i));
+
                 stack.push(s.charAt(i));
                 continue;
             }
-            if (stack.isEmpty())
+            System.out.println(stack);
+
+            if (stack.isEmpty()) {
                 return false;
-
-            if (s.charAt(i) == '}' && stack.peek() == '{') {
-                stack.pop();
-                continue;
             }
-            if (s.charAt(i) == ']' && stack.peek() == '[') {
-                stack.pop();
-                continue;
-
+            if (s.charAt(i) == ')') {
+                System.out.println("Compared: " + s.charAt(i) + " Item popped: " + stack.peek());
+                if (stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
             }
-            if (s.charAt(i) == ')' && stack.peek() == '(') {
-                stack.pop();
-                continue;
-
-            }
-            return false;
+            if (s.charAt(i) == ']') {
+                System.out.println("Compared: " + s.charAt(i) + " Item popped: " + stack.peek());
+                if (stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+             }
+            if (s.charAt(i) == '}') {
+                System.out.println("Compared: " + s.charAt(i) + " Item popped: " + stack.peek());
+                if (stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+             }
         }
         if (stack.isEmpty()) {
             return true;
-        } else {
-            return false;
         }
+        return false;
+
     }
 }
