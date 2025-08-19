2ea281a04546e47de6fe1ff65cac4c160f40707b
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-19 13:16:06 +0530

    File updated at 19/08/2025, 13:16:05

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,23 +1,19 @@
-// Last updated: 19/08/2025, 13:15:35
+// Last updated: 19/08/2025, 13:16:05
 class Solution {
     public boolean isValid(String s) {
         int len = s.length();
         Stack<Character> stack = new Stack();
         for (int i = 0; i < len; i++) {
-            System.out.println("*************");
             if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-                System.out.println("Item pushed: " + s.charAt(i));
 
                 stack.push(s.charAt(i));
                 continue;
             }
-            System.out.println(stack);
 
             if (stack.isEmpty()) {
                 return false;
             }
             if (s.charAt(i) == ')') {
-                System.out.println("Compared: " + s.charAt(i) + " Item popped: " + stack.peek());
                 if (stack.peek() == '(') {
                     stack.pop();
                 } else {
@@ -25,21 +21,19 @@ class Solution {
                 }
             }
             if (s.charAt(i) == ']') {
-                System.out.println("Compared: " + s.charAt(i) + " Item popped: " + stack.peek());
                 if (stack.peek() == '[') {
                     stack.pop();
                 } else {
                     return false;
                 }
-             }
+            }
             if (s.charAt(i) == '}') {
-                System.out.println("Compared: " + s.charAt(i) + " Item popped: " + stack.peek());
                 if (stack.peek() == '{') {
                     stack.pop();
                 } else {
                     return false;
                 }
-             }
+            }
         }
         if (stack.isEmpty()) {
             return true;
