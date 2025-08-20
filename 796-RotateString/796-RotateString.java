8c5b10fa947c8e9ff3cbe76e79ea1d070cbba0be
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-21 01:05:26 +0530

    File updated at 21/08/2025, 01:05:25

--- 796-RotateString/796-RotateString.java
+++ 796-RotateString/796-RotateString.java
@@ -1,23 +1,17 @@
-// Last updated: 21/08/2025, 01:05:01
+// Last updated: 21/08/2025, 01:05:25
 class Solution {
     public String reverseWords(String s) {
         String[] arr = s.split(" ");
-        for (String str : arr) {
-            System.out.println(str);
-        }
         StringBuilder sb = new StringBuilder();
-        System.out.println(sb);
-        for (int i = arr.length - 1; i >= 0; i--) {
+         for (int i = arr.length - 1; i >= 0; i--) {
             if (arr[i].length() < 1) {
                 continue;
             }
             sb.append(arr[i].trim());
             sb.append(" ");
-            System.out.println("***** " + sb + "***** ");
-
+ 
         }
-        System.out.println("FINAL::: " + sb + "***** ");
-
+ 
         return sb.toString().trim();
     }
 }
