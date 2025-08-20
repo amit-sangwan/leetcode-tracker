cfac20844d5be14da654c5d86cd48af5c87eb518
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-20 23:30:01 +0530

    File updated at 20/08/2025, 23:30:00

--- 1116-PrintZeroEvenOdd/1116-PrintZeroEvenOdd.java
+++ 1116-PrintZeroEvenOdd/1116-PrintZeroEvenOdd.java
@@ -1,13 +1,6 @@
-// Last updated: 20/08/2025, 23:27:21
+// Last updated: 20/08/2025, 23:30:00
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
-        /**
-        Sliding Window - Two Pointer Approach
-        System.out.println("dubbuger");
-         
-        Input: abcabcbb
-         */
 
         int len = s.length();
         if (len == 0) {
@@ -20,29 +13,23 @@ class Solution {
 
         for (int rightPointer = 0; rightPointer < len; rightPointer++) {
 
-            if (!set.contains(s.charAt(rightPointer))) {
-                set.add(s.charAt(rightPointer));
-              //  System.out.println("char added " + s.charAt(rightPointer) + " set become: " + set);
-                target = Math.max(target, rightPointer - leftPointer + 1);
+       //     if (!set.contains(s.charAt(rightPointer))) {
+    
 
-                continue;
-            }
+         //       continue;
+        //    }
 
             while (set.contains(s.charAt(rightPointer))) {
                 set.remove(s.charAt(leftPointer));
                 leftPointer++;
-             //   System.out.println("In while , removed : " + s.charAt(leftPointer));
 
             }
-         //   System.out.println("set after , removed : " + set);
-
             set.add(s.charAt(rightPointer));
-         //   System.out.println("set adding 1   : " + set);
+                target = Math.max(target, rightPointer - leftPointer + 1);
+            set.add(s.charAt(rightPointer));
 
             target = Math.max(target, rightPointer - leftPointer + 1);
-        //    System.out.println("Target : " + target);
-
-            System.out.println("***** Loop Ended ****");
+          
 
         }
 
