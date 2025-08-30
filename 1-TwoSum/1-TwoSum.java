785181a051815e94a3d29ee1f83db853148da486
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-31 00:17:08 +0530

    File updated at 31/08/2025, 00:17:08

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,21 +1,21 @@
-// Last updated: 31/08/2025, 00:16:38
+// Last updated: 31/08/2025, 00:17:08
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
         int len = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
-        System.out.println("Map initiated: " + map);
+       // System.out.println("Map initiated: " + map);
         for (int i = 0; i < len; i++) {
 
             int need = target - nums[i];
-            System.out.println("Current number: " + nums[i] + " Number Required: " + (target - nums[i]) );
-            System.out.println(map.containsKey(need));
+      //      System.out.println("Current number: " + nums[i] + " Number Required: " + (target - nums[i]) );
+       //     System.out.println(map.containsKey(need));
             if (map.containsKey(need)) {
 
                 return new int[] { i, map.get(need) };
             }
             map.put(nums[i], i);
-            System.out.println("********* End of Loop Cycle *******");
+        //    System.out.println("********* End of Loop Cycle *******");
         }
         return new int[] { 0, 0 };
 
