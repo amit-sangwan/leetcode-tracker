27c501167ad54283087c389d012d589df6cc233a
Amit Sangwan <52357167+amit-sangwan@users.noreply.github.com>
2025-08-31 01:03:31 +0530

    File updated at 31/08/2025, 01:03:31

--- 2309-GreatestEnglishLetterinUpperandLowerCase/2309-GreatestEnglishLetterinUpperandLowerCase.java
+++ 2309-GreatestEnglishLetterinUpperandLowerCase/2309-GreatestEnglishLetterinUpperandLowerCase.java
@@ -1,12 +1,11 @@
-// Last updated: 31/08/2025, 01:03:15
+// Last updated: 31/08/2025, 01:03:31
 class Solution {
     public String greatestLetter(String s) {
          Set<Character> set = new HashSet<>();  // HashSet<Character> set = new HashSet<>(); -> HashSet H and S in caps
         for (char c : s.toCharArray()) {       // Character
             set.add(c);
         }
-        System.out.print(set);
-        for (char z = 'Z'; z >= 'A'; z--) {
+         for (char z = 'Z'; z >= 'A'; z--) {
 
             if (set.contains(z) && set.contains(Character.toLowerCase(z))) {
                 return Character.toString(z) ;
